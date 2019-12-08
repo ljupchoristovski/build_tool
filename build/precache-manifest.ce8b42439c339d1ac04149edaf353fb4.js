69cc8205045fc1a95f6be9ba324008a681304f8b
ljupcho <rljupcho@gmail.com>
2019-12-08 20:14:13 +0100

    build version

---
+++ build/precache-manifest.ce8b42439c339d1ac04149edaf353fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6efae938bf262803870b829bdd7a7d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a17ec1c677d9e94f7cd",
+    "url": "./static/css/main.f2aad5d9.chunk.css"
+  },
+  {
+    "revision": "bea75747de00095facc6",
+    "url": "./static/js/2.416968fa.chunk.js"
+  },
+  {
+    "revision": "7a17ec1c677d9e94f7cd",
+    "url": "./static/js/main.4f0d2bc4.chunk.js"
+  },
+  {
+    "revision": "5b27ff80b5c920a98fd8",
+    "url": "./static/js/runtime-main.03d47b47.js"
+  }
+]);
